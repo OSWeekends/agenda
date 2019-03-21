c02874242351fdd198ef58832db674aa1945021e
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-03-21 23:48:56 +0100

    Footer

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="text-center">
+    <p>Made with 💙 by OSWeekends Guilders 🦄</p>
+    <p>
+      <b-button variant="link" @click="goUrl('https://github.com/OSWeekends/agenda?ref=agenda')">Github</b-button>
+      <b-button variant="link" @click="goUrl('https://osweekends.com/?ref=agenda')">Open Source Weekends</b-button>
+      <b-button variant="link" @click="goUrl('https://invitations-osweekends.herokuapp.com/?ref=agenda')">Slack</b-button>
+    </p>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'BaseFooter',
+  methods: {
+    goUrl (href) {
+      Object.assign(document.createElement('a'), {
+        target: '_blank',
+        href
+      }).click()
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,22 @@
   <div class="home">
     <BaseHeader :title="data.title"/>
     <ScheduleList :tracks="data.tracks" />
+    <BaseFooter />
   </div>
 </template>
 
 <script>
 import BaseHeader from '@/components/BaseHeader.vue'
 import ScheduleList from '@/components/ScheduleList.vue'
+import BaseFooter from '@/components/BaseFooter.vue'
 import data from '@/data/index'
 
 export default {
   name: 'home',
   components: {
     BaseHeader,
-    ScheduleList
+    ScheduleList,
+    BaseFooter
   },
   data () {
     return {
