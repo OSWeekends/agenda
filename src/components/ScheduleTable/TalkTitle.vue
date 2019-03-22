c9f8d8f5ae5eb01d6c4298f149389b71fa652924
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-03-22 19:23:21 +0100

    Talk title

---
+++ src/components/ScheduleTable/TalkTitle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h4>
+      <b-badge v-if="type" :variant="badgeVariant(type)"> · </b-badge>
+      {{ title }}
+    </h4>
+  </div>
+</template>
+
+<script>
+const variants = [
+  'primary',
+  'success',
+  'danger'
+]
+
+export default {
+  props: {
+    type: {
+      required: false
+    },
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    badgeVariant (type) {
+      return variants[type - 1]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
