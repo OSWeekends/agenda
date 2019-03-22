09d572bc453921309155f48091b1e276ef3c7806
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-03-22 20:23:38 +0100

    Bio

--- src/components/ScheduleTable/TalkAuthorsBio.vue
+++ src/components/ScheduleTable/TalkAuthorsBio.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-
+    <p class="lead text-muted" v-for="(item, index) in bio" :key="index">
+      {{ item }}
+    </p>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'AuthorsBio',
+  props: {
+    bio: {
+      type: Array
+    }
+  }
 }
 </script>
 
