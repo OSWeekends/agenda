7df93f5b63835178c999f5a8a7eecc038c60dd2c
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-12-02 01:00:25 +0100

    Agenda dic 2019

--- src/data/index.js
+++ src/data/index.js
@@ -304,7 +304,7 @@ const data = {
         {
           title: 'Advanced JavaScript Patterns',
           type: 'Charla',
-          variant: 'warning',
+          tags: ['javascript', 'tricks'],
           description: [
             'When you tell people you code in JavaScript they usually look down on you. This talk will give you the tools to enlighten those heretics.',
             'We\'ll be entering the world of design patterns applied to JavaScript, looking through some of the cutting edge properties of ES6 such as Proxies and default function parameter expressions, dealing with objects and even some examples of functional programming. ' +
@@ -331,7 +331,8 @@ const data = {
           endTime: {
             h: '13',
             m: '00'
-          }
+          },
+          variant: 'warning'
         }
       ]
     }
