0e39ed52411c6b6a7155cc23af73fb93445808ea
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-06-22 21:20:43 +0200

    Agenda Junio 20 #2

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="d-flex justify-content-between align-items-center">
+      <h1 class="mt-4">About</h1>
+      <div class="h-100">
+        <b-button variant="link" to="/">← Back</b-button>
+      </div>
+    </div>
+    <hr>
+    <p class="lead">
+      This website is built with <a href="https://vuejs.org/">Vue.js</a> and <a href="https://bootstrap-vue.org/">BootstrapVue</a> and is hosted in
+      <a href="https://www.netlify.com/">Netlify</a>.
+    </p>
+    <p class="lead">Source code is here: <a href="https://github.com/OSWeekends/agenda?ref=agenda">https://github.com/OSWeekends/agenda</a></p>
   </div>
 </template>
