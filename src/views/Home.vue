4904d87a783cdeb0b7fa6f77341b9f8030bc60f6
thamaragerigr <thamaragerigr@gmail.com>
2020-10-17 00:07:10 +0200

    added twitch link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,17 @@
   <div class="home">
     <BaseHeader :headerData="headerData" />
     <ScheduleList :tracks="data.tracks"/>
+    <div class="text-center mb-5 d-flex flex-column">
+      <p>Síguenos en</p>
+      <div>
+        <a class="twitch-icon mb-3" href="https://www.twitch.tv/osweekends">
+          <font-awesome-icon :icon="['fab', 'twitch']" size="5x" />
+        </a>
+      </div>
+      <a class="twitch-link" href="https://www.twitch.tv/osweekends">
+        twitch.tv/osweekends
+      </a>
+    </div>
     <BaseFooter/>
   </div>
 </template>
@@ -39,3 +50,18 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+.twitch-icon
+  color #3F414E
+  background-color white
+  padding 35px 40px
+  border-radius 50%
+  align-items center
+  display inline-block
+.twitch-link
+  color #3F414E
+  font-size 24px
+  font-weight bolder
+  text-decoration none
+</style>
