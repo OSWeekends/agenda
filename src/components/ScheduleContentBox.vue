fe343df59954e38dc37d3f42849e424621716c78
thamaragerigr <thamaragerigr@gmail.com>
2020-10-21 22:48:55 +0200

    display center for when theres only titles

--- src/components/ScheduleContentBox.vue
+++ src/components/ScheduleContentBox.vue
@@ -1,6 +1,7 @@
 <template>
   <b-col>
     <div class="content-box bg-white rounded-lg shadow-sm">
+       <div v-if="content.description && content.speakers">
       <!-- Charla -->
       <div>
         <header>
@@ -17,6 +18,13 @@
       <template v-if="content.speakers">
         <SpeakersInfo :speakers="content.speakers"/>
       </template>
+      </div>
+
+       <div v-else class="text-center">
+        <header>
+          <h4 class="font-weight-bold">{{ content.title }}</h4>
+        </header>
+      </div>
     </div>
   </b-col>
 </template>
