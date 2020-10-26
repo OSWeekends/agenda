7443407fb185add11ec737378c73642c300f2f9f
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-10-26 16:43:13 +0100

    Update indent

--- src/components/MeetupLink.vue
+++ src/components/MeetupLink.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="text-center mb-5">
-      <div v-if="eventType === 'Online'">
-        <p>Síguenos en</p>
-          <a class="link mb-3" href="https://www.twitch.tv/osweekends">
-                <div  class="icon bg-white rounded-circle d-flex justify-content-center align-items-center mb-3">
-                    <font-awesome-icon :icon="['fab', 'twitch']" size="5x" />
-                </div>
-            <span class="address">twitch.tv/osweekends</span>
-          </a>
-      </div>
+    <div v-if="eventType === 'Online'">
+      <p>Síguenos en</p>
+      <a class="link mb-3" href="https://www.twitch.tv/osweekends">
+        <div class="icon bg-white rounded-circle d-flex justify-content-center align-items-center mb-3">
+          <font-awesome-icon :icon="['fab', 'twitch']" size="5x"/>
+        </div>
+        <span class="address">twitch.tv/osweekends</span>
+      </a>
+    </div>
 
-      <div v-else>
-           <a class="link mb-3" href="https://www.google.com/search?sxsrf=ALeKk01W0wUmU-ubU9z2pfgRFZY50MntHw:1602887276901&q=google+campus&npsic=0&rflfq=1&rlha=0&rllag=40412472,-3718242,10&tbm=lcl&ved=2ahUKEwigpYS2lLrsAhWPA2MBHf6JCucQtgN6BAgGEAc&rldoc=1#rlfi=hd:;si:18186252069890377247,l,Cg1nb29nbGUgY2FtcHVzIgOIAQFaHgoNZ29vZ2xlIGNhbXB1cyINZ29vZ2xlIGNhbXB1cw;mv:[[40.4382547,-3.6678058],[40.4109177,-3.7211292]]">
-                <div  class="icon bg-white rounded-circle d-flex justify-content-center align-items-center mb-3">
-                     <font-awesome-icon :icon="['fas', 'map-marker-alt']" size="5x" />
-                </div>
-            <span class="address">C. de Moreno Nieto, 2, 28005 Madrid</span>
-          </a>
-      </div>
+    <div v-else>
+      <a class="link mb-3"
+         href="https://www.google.com/search?sxsrf=ALeKk01W0wUmU-ubU9z2pfgRFZY50MntHw:1602887276901&q=google+campus&npsic=0&rflfq=1&rlha=0&rllag=40412472,-3718242,10&tbm=lcl&ved=2ahUKEwigpYS2lLrsAhWPA2MBHf6JCucQtgN6BAgGEAc&rldoc=1#rlfi=hd:;si:18186252069890377247,l,Cg1nb29nbGUgY2FtcHVzIgOIAQFaHgoNZ29vZ2xlIGNhbXB1cyINZ29vZ2xlIGNhbXB1cw;mv:[[40.4382547,-3.6678058],[40.4109177,-3.7211292]]">
+        <div class="icon bg-white rounded-circle d-flex justify-content-center align-items-center mb-3">
+          <font-awesome-icon :icon="['fas', 'map-marker-alt']" size="5x"/>
+        </div>
+        <span class="address">C. de Moreno Nieto, 2, 28005 Madrid</span>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -37,17 +38,20 @@ export default {
 main-blue = #003DA5
 .link
   display inline-block
+
   .icon
     color #3F414E
     width 200px
     height 200px
     margin auto
+
   .address
     color #3F414E
     font-size 24px
     font-weight bolder
-   text-decoration none
-   &:hover
-     color #003DA5
-     text-decoration none
+    text-decoration none
+
+  &:hover
+    color #003DA5
+    text-decoration none
 </style>
--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="sponsors-section">
     <p>Patrocinado por:</p>
-     <b-row class="sponsors">
+    <b-row class="sponsors">
       <b-col v-for="sponsor in sponsors" :key="sponsor.name">
         <a :href="sponsor.link" class="text-center">
           <img src="../assets/sponsors/spotify.svg" :alt="sponsor.name">
         </a>
-    </b-col>
+      </b-col>
     </b-row>
   </div>
 </template>
@@ -26,6 +26,7 @@ export default {
 <style lang="stylus">
 .sponsors-section
   color #898B94
+
   .sponsors
     text-align center
     max-width 620px
