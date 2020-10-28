d97ad01ad638f4adcca1f7fcbde2524d4c9d9247
thamaragerigr <thamaragerigr@gmail.com>
2020-10-28 18:23:22 +0100

    added new font

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
 </template>
 
 <style lang="stylus">
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@700&family=Open+Sans:wght@400;600;700&display=swap');
+
 body
   margin 0
   background-color #F9FAFC !important
 
 #app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
+   font-family 'Open Sans', sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   color #2c3e50
